32f0851dc26e8c459430ed48b11ac57e1232caad
Sylvain Auguy <s.auguy@gmail.com>
2018-08-19 18:08:41 +0200

    rename api_configs.yml to third_party_apis.yml

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,6 @@
 API_CONFIGS =
-  YAML.load_file(File.join(Rails.root, 'config', 'api_configs.yml')).freeze
+  YAML.load_file(File.join(Rails.root, 'config', 'third_party_apis.yml')).freeze
+
 ACCESS_CREDENTIALS = {
   login: Rails.application.credentials.basic_authentication[:login],
   password: Rails.application.credentials.basic_authentication[:password]
